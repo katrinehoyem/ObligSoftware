11bdd51ab9e1c966df49f918b5a21202d0c285cf
Katrine Høyem <89635012+katrinehoyem@users.noreply.github.com>
2021-11-09 18:25:35 +0100

    added class leap_year_test

---
+++ untitled/src/test/java/leap_year_test.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class leap_year_test {
+
+    @Test
+    public void year_divisible_by_4_not_100(){
+        assertTrue(leap_year.is_leap_year(16));
+    }
+    @Test
+    public void year_divisible_by_400(){
+        assertTrue(leap_year.is_leap_year(2000));
+    }
+    @Test
+    public void year_not_divisible_by_4(){
+        assertFalse(leap_year.is_leap_year(1995));
+    }
+    @Test
+    public void year_divisible_by_100_not_400(){
+        assertFalse(leap_year.is_leap_year(2015));
+    }
+
+
+}
