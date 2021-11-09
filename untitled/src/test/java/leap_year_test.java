88f4fbbe034aee1a6ed724e964bd5bb491a8f338
katrinehoh <katrinehoyem@hotmail.com>
2021-11-09 19:36:00 +0100

    everything works

--- untitled/src/test/java/leap_year_test.java
+++ untitled/src/test/java/leap_year_test.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 import static org.junit.Assert.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import static org.junit.Assert.assertTrue;
 
 public class leap_year_test {
 
